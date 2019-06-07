79230b5590ab4e19590e4c2c0684f4cbfe46c08f
Arif Yayalar <ayayalar@mac.com>
2019-06-07 12:50:28 -0700

    Identity Server Demo

---
+++ Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = client.GetDiscoveryDocumentAsync("https://localhost:5000").Result;
+
+            if (!disco.IsError)
+            {
+                var token = client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+                {
+                    Address = disco.TokenEndpoint,
+                    ClientId = "apiClient",
+                    ClientSecret = "secret",
+                    Scope = "api1"
+                }).Result;
+
+                if (!token.IsError)
+                {
+                    Console.WriteLine(token.Json);
+
+                    client = new HttpClient();
+                    client.SetBearerToken(token.AccessToken);
+
+                    var response = client.GetAsync("https://localhost:5001/api/identity").Result;
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine(response.StatusCode);
+                    }
+                    else
+                    {
+                        var content = response.Content.ReadAsStringAsync().Result;
+                        Console.WriteLine(JArray.Parse(content));
+                    }
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IdServer/Config.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "api1")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"api1"},
+                    ClientId = "apiClient",
+                    ClientSecrets = { new Secret("secret".Sha256()) }
+                }
+            };
+        }
+    }
+}
